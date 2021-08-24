340f5444d0c2d834c9262da1522dfb3fd79d1a9c
eli <eli@assumptioncollege.edu>
2021-08-24 03:14:02 +0200

    Cleaned up unused files and formatted wacky JS/json

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const http = require('http'),
     config = require('./config.json'),
     Corrosion = require('corrosion'),
     proxy = new Corrosion({
-	      prefix: config.prefix,
+        prefix: config.prefix,
         codec: 'xor'
     }),
     app = (req, res) => {
-      if (req.url.startsWith(config.prefix)) return proxy.request(req, res);
-      req.pathname = req.url.split('#')[0].split('?')[0];
-      req.query = {};
-      const publicPath = __dirname + '/public' + req.pathname;
-      mime.contentType(publicPath);
-      const error = () => (res.statusCode = 404, res.end(fs.readFileSync(__dirname + '/error.html', 'utf-8').replace('%ERR%', `Cannot ${req.method} ${req.pathname}`)))
-      fs.lstat(publicPath, (err, stats) => {
-        if (err) return error();
-        res.setHeader("Content-Type", mime.lookup(req.pathname)), res.writeHead(200), stats.isDirectory() ? fs.existsSync(publicPath + 'index.html') ? fs.createReadStream(publicPath + 'index.html').pipe(res) : error() : !stats.isFile() || publicPath.endsWith("/") ? error() : fs.createReadStream(publicPath).pipe(res);
-      });
+        if (req.url.startsWith(config.prefix)) return proxy.request(req, res);
+        req.pathname = req.url.split('#')[0].split('?')[0];
+        req.query = {};
+        const publicPath = __dirname + '/public' + req.pathname;
+        mime.contentType(publicPath);
+        const error = () => (res.statusCode = 404, res.end(fs.readFileSync(__dirname + '/error.html', 'utf-8').replace('%ERR%', `Cannot ${req.method} ${req.pathname}`)))
+        fs.lstat(publicPath, (err, stats) => {
+            if (err) return error();
+            res.setHeader("Content-Type", mime.lookup(req.pathname)), res.writeHead(200), stats.isDirectory() ? fs.existsSync(publicPath + 'index.html') ? fs.createReadStream(publicPath + 'index.html').pipe(res) : error() : !stats.isFile() || publicPath.endsWith("/") ? error() : fs.createReadStream(publicPath).pipe(res);
+        });
     },
     server = config.ssl ? https.createServer({
-      key: fs.readFileSync("./ssl/default.key"),
-      cert: fs.readFileSync("./ssl/default.crt")
+        key: fs.readFileSync("./ssl/default.key"),
+        cert: fs.readFileSync("./ssl/default.crt")
     }, app) : http.createServer(app);
 
-server.listen(process.env.PORT || config.port, ()=>console.log(`${config.ssl ? 'https://' : 'http://'}0.0.0.0:${config.port}`));
+server.listen(process.env.PORT || config.port, () => console.log(`${config.ssl ? 'https://' : 'http://'}0.0.0.0:${config.port}`));
