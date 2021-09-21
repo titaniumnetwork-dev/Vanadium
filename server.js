bc4f44fdac144c358b6ecca8bd74e3d03a785460
Null N Void Server <nullnvoid@mailfence.com>
2021-09-21 16:18:51 +0000

    getting it converted to express for cleaner code and actually functioning ssl

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const http = require('http'),
     https = require('https'),
     fs = require('fs'),
-    mime = require('mime-types'),
     config = require('./config.json'),
     Corrosion = require('corrosion'),
+    express = require('express'),
     proxy = new Corrosion({
         prefix: config.prefix,
         codec: 'xor'
     }),
-    app = (req, res) => {
+    app = express(),
+    gamermode = (req, res) => {
         if (req.url.startsWith(config.prefix)) return proxy.request(req, res);
         req.pathname = req.url.split('#')[0].split('?')[0];
-        req.query = {};
-        const publicPath = __dirname + '/public' + req.pathname;
-        mime.contentType(publicPath);
-        const error = () => (res.statusCode = 404, res.end(fs.readFileSync(__dirname + '/error.html', 'utf-8').replace('%ERR%', `Cannot ${req.method} ${req.pathname}`)))
-        fs.lstat(publicPath, (err, stats) => {
-            if (err) return error();
-            res.setHeader("Content-Type", mime.lookup(req.pathname)), res.writeHead(200), stats.isDirectory() ? fs.existsSync(publicPath + 'index.html') ? fs.createReadStream(publicPath + 'index.html').pipe(res) : error() : !stats.isFile() || publicPath.endsWith("/") ? error() : fs.createReadStream(publicPath).pipe(res);
-        });
-    },
-    server = config.ssl ? https.createServer({
-        key: fs.readFileSync("./ssl/default.key"),
-        cert: fs.readFileSync("./ssl/default.crt")
-    }, app) : http.createServer(app);
+        req.query = {};};
+app.use('/', express.static(__dirname + '/public'));
+app.use('/', gamermode);
 
-server.listen(process.env.PORT || config.port, () => console.log(`${config.ssl ? 'https://' : 'http://'}0.0.0.0:${config.port}`));
+//This is the simple HTTP server. Remove the HTTPS server and use this if you encounter errors.
+//
+//app.listen(config.port, () => {console.log(`Hacker proxy running at http://localhost:${config.port}`)});
+ 
+// This is the HTTPS server.
+https.createServer({
+	  key: fs.readFileSync('./ssl/default.key'),
+	cert: fs.readFileSync('./ssl/default.crt')}, app)
+	.listen(config.port, function () {console.log(`Hacker proxy running at http://localhost:${config.port}`)});
+//Remove this if self-signed SSL is a problem for you.
