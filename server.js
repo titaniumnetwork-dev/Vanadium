916cffa0dddca327f6c6ab80a4d3680e74744a85
LQ16 <nullnvoid@mailfence.com>
2022-01-26 21:24:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http'),
     https = require('https'),
-    fs = require('fs'),
     config = require('./config.json'),
     Corrosion = require('corrosion'),
     express = require('express'),
@@ -9,11 +8,11 @@ const http = require('http'),
         codec: 'xor'
     }),
     app = express(),
-    gamermode = (req, res) => {
+    proxyservice = (req, res) => {
         if (req.url.startsWith(config.prefix)) return proxy.request(req, res);
         req.pathname = req.url.split('#')[0].split('?')[0];
         req.query = {};};
 app.use('/', express.static(__dirname + '/public'));
-app.use('/', gamermode);
+app.use('/', proxyservice);
 // Simple HTTP server.
 app.listen(process.env.PORT || config.port, () => {console.log(`Vanadium running at http://localhost:${config.port}`)});
