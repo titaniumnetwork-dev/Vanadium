50f2662ad0bda41a00e42a20eb8f5b20ef01e529
NullnVoid <NullnVoid@mailfence.com>
2022-05-09 23:45:49 -0400

    changing to /service/

--- public/assets/main.js
+++ public/assets/main.js
@@ -21,7 +21,7 @@ try {
         elemadd = (a, b, c) => Object.assign(b.appendChild(document.createElement(a)), c);
     // Search Suggestions
     search.addEventListener("input", () => {
-            fetch("/search/" + xor.encode(`${searchprovider}ac/?q=${search.value}`)).then((a) => a.text()).then((a) => JSON.parse(`[${a.split("\n")[5].slice(0, -1)}]`)[0]).then((a) => {
+            fetch("/service/" + xor.encode(`${searchprovider}ac/?q=${search.value}`)).then((a) => a.text()).then((a) => JSON.parse(`[${a.split("\n")[5].slice(0, -1)}]`)[0]).then((a) => {
                 (suggelem.innerHTML = ""), a.forEach((a) => elemadd("option", suggelem, {
                     className: "search-opt",
                     value: a.phrase
@@ -29,7 +29,7 @@ try {
             });
         }),
         // Submission and format detection
-        document.getElementById("searchform").addEventListener("submit", () => {event.preventDefault(),a=search.value,location.assign(/^(?:https?:\/\/)?\w+\.\w/.test(a)?/^(https?|ftp):/.test(a)?"/search/"+xor.encode(a)+"/":"/search/"+xor.encode("https://"+a)+"/":"/search/hvtrs8%2F-dwcidwcigm.aoo/?q="+a);});
+        document.getElementById("searchform").addEventListener("submit", () => {event.preventDefault(),a=search.value,location.assign(/^(?:https?:\/\/)?\w+\.\w/.test(a)?/^(https?|ftp):/.test(a)?"/service/"+xor.encode(a)+"/":"/service/"+xor.encode("https://"+a)+"/":"/service/hvtrs8%2F-dwcidwcigm.aoo/?q="+a);});
 } catch {
     // Catch Media Page
     console.log("Could not load Index elements, assuming user is on media or media frame.");
